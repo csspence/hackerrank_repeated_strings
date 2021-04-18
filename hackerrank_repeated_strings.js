2281bb9c3ce7a4ad0f93106bfd7453e085143098
Crew Spence <csspence123@gmail.com>
2021-04-18 05:57:33 -0500

    Wrote naive solution.

--- hackerrank_repeated_strings.js
+++ hackerrank_repeated_strings.js
@@ -2,7 +2,21 @@
 HackerRank warmup problem
 */
 
-function repeatedString(s, n) {
+const repeatedString = (s, n) => {
+  let index = 0;
+  let spacesCovered = 0;
+  let counter = 0;
+  while(spacesCovered < n) {
+    if(s[index] === 'a') {
+      counter++;
+    }
+    if(index === s.length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    spacesCovered++;
+  }
 
-
+  return counter;
 }
